4ef37c3635810b689da24163e63b0f2fa017e570
Pawel Mstowski <pawel.mstowski@gmail.com>
2019-04-30 22:01:49 +0200

    node rest api with parcel middleware for handling frontend

--- src/bundle.js
+++ src/bundle.js
@@ -1,1 +1,7 @@
+/* eslint-disable no-console */
 import "./style.scss";
+
+import axios from "axios";
+
+axios.get(`${window.location.protocol}//${window.location.host}/message`)
+    .then(res => console.log(res.data));
---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// eslint-disable-next-line no-console
+console.log(__dirname);
+
+const Bundler = require("parcel-bundler");
+
+app.get("/message", (req, res) => {
+    // eslint-disable-next-line no-console
+    console.log("reuest!!!");
+    res.json("message from server");});
+
+    
+app.use(new Bundler(`${__dirname}/index.html`, {watch: true, sourceMaps: true}).middleware());
+
+
+// eslint-disable-next-line no-console
+app.listen(port, () => console.log(`Listening on port: ${port}`));
